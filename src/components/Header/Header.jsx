e55d1fbadb56940e7f7671effe2de555259ddf89
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-12-09 18:45:19 +0000

    feat: pass score as prop to scoreboard component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import '../../styles/Header.css'
+import Scoreboard from './Scoreboard';
 
-export default function Header() {
+export default function Header({ score }) {
   return (
     <header>
       <h1 className="header-logo">Memory Card Game</h1>
+      <Scoreboard score={score} />
     </header>
   );
 }
