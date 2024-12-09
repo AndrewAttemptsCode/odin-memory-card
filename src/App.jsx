033908102e507c63464e2f6ae8d9499fb0fea18c
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-12-09 18:44:26 +0000

    feat: add score state and pass as props to header and main

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import Main from './components/Main/Main'
 
 export default function App() {
+  const [score, setScore] = useState(0);
 
   return (
     <>
-      <Header />
-      <Main />
+      <Header score={score} />
+      <Main setScore={setScore} />
     </>
   )
 }
