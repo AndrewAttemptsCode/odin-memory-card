3868315c0335c73751798a002d8624100a807ca5
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-12-10 18:49:31 +0000

    feat: add container for logo and game instructions

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,10 @@ import Scoreboard from './Scoreboard';
 export default function Header({ score, bestScore }) {
   return (
     <header>
-      <h1 className="header-logo">Memory Card Game</h1>
+      <div className='logo-container'>
+        <h1 className="header-logo">Memory Card Game</h1>
+        <p>Get points by clicking on an image, but don't click on any more than once!</p>
+      </div>  
       <Scoreboard score={score} bestScore={bestScore} />
     </header>
   );
