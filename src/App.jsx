3d1681c8d20e72ae390f9f521143f245032deaa1
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-12-10 15:03:57 +0000

    feat: add best score tracking state

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import Main from './components/Main/Main'
 
 export default function App() {
   const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
 
   return (
     <>
-      <Header score={score} />
-      <Main setScore={setScore} />
+      <Header score={score} bestScore={bestScore} />
+      <Main score={score} setScore={setScore} bestScore={bestScore} setBestScore={setBestScore} />
     </>
   )
 }
