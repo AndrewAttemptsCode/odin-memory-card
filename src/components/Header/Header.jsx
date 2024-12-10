a39416a133c103709fa09b41eb1edb4be6a3b27c
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-12-10 15:04:26 +0000

    feat: pass best score prop

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import '../../styles/Header.css'
 import Scoreboard from './Scoreboard';
 
-export default function Header({ score }) {
+export default function Header({ score, bestScore }) {
   return (
     <header>
       <h1 className="header-logo">Memory Card Game</h1>
-      <Scoreboard score={score} />
+      <Scoreboard score={score} bestScore={bestScore} />
     </header>
   );
 }
