1b3874259c7eea7fd63b45d9cf54980b463aa8e5
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-12-10 13:23:14 +0000

    feat: add state and effect for score update text animation

--- src/components/Header/Scoreboard.jsx
+++ src/components/Header/Scoreboard.jsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from 'react';
+import '../../styles/Scoreboard.css';
+
 export default function Scoreboard({ score }) {
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  useEffect(() => {
+    setIsAnimating(true);
+    const timeout = setTimeout(() => setIsAnimating(false), 200);
+    return () => clearTimeout(timeout);
+  }, [score])
+
   return (
     <div className="scoreboard">
-      <p>Score: {score}</p>
+      <p>Score: <span className={`score-display ${isAnimating ? "animate" : ""}`}>{score}</span></p>
       <p>Best Score:</p>
     </div>
   );
